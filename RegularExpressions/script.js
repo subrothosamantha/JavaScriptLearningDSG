1a2872caab42cbb2c1296afdb74805bb8a2ad3ac
subrotho samantha <subrotho.samantha@dunnsolutions.com>
2022-02-24 14:30:09 +0530

    learned about RegularExpression

---
+++ RegularExpressions/script.js
@@ -0,0 +1,27 @@
+//flags
+//g - global
+//i - case insensitivity
+
+let reg = /From/i;
+
+let s = 'This regular expression I learned from code with harry';
+
+//will return if present or null
+let result = reg.exec(s);
+// console.log(result.input);
+
+//will return true of false
+let result1 = reg.test(s);
+// console.log(result1);
+
+//match will return an array of result or null
+let result3 = s.match(reg);
+// console.log(result3);
+
+//search() will search for a word
+
+let result4 = s.search(reg);
+// console.log(result4)
+
+let result5 = s.replace(reg,'subroto');
+console.log(result5)
